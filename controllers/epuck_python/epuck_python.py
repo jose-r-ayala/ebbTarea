12b7c840079e91a253532ba3ea74881339b1b12e
jose-r-ayala <jose.ayala.personal@gmail.com>
2025-05-27 16:46:08 -0600

    infinite movement

--- controllers/epuck_python/epuck_python.py
+++ controllers/epuck_python/epuck_python.py
@@ -1,6 +1,11 @@
-from controller import Robot, Motor
+from controller import Robot
 
 TIME_STEP = 64
+MAX_SPEED = 6.28
+DISTANCE_SENSORS_NUMBER = 8
+
+DISTANCE_SENSORS_NAMES = ["ps0", "ps1", "ps2", "ps3", "ps4", "ps5", "ps6", "ps7"]
+DISTANCE_SENSORS = []
 
 # create the Robot instance.
 robot = Robot()
@@ -9,8 +14,13 @@ robot = Robot()
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 # set the target position of the motors
-leftMotor.setPosition(10.0)
-rightMotor.setPosition(10.0)
+leftMotor.setPosition(float('+inf'))
+rightMotor.setPosition(float('+inf'))
+leftMotor.setVelocity(MAX_SPEED)
+rightMotor.setVelocity(MAX_SPEED)
 
+# IMPORTANT this functions allows the robot to move
 while robot.step(TIME_STEP) != -1:
    pass
+
+# INITIALIZE SENSORS & LEDS
